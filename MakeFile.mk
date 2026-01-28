ca923edf83d50a8d3691139b8822fe94ddd6f708
Fortunatah <haydenjohnfortunata@gmail.com>
2026-01-27 19:07:02 -0500

    Added system clear

--- MakeFile.mk
+++ MakeFile.mk
@@ -7,7 +7,7 @@ INCLUDE = include
 all: $(BUILD)/main
 
 $(BUILD)/main: $(BUILD)/main.o $(BUILD)/get_system_info.o
-	gcc $(CFLAGS) $(BUILD)/main.o -o $(BUILD)/main
+	gcc $(CFLAGS) $(BUILD)/*.o -o $(BUILD)/main
 
 $(BUILD)/main.o: $(SRC)/main.c | $(BUILD)
 	gcc $(CFLAGS) -c $(SRC)/main.c -o $(BUILD)/main.o
