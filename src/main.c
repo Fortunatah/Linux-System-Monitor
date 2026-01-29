b544836e6a6680392ed20e52d8830897eea8391b
Fortunatah <haydenjohnfortunata@gmail.com>
2026-01-29 18:42:13 -0500

    Added cpuinfo

--- src/main.c
+++ src/main.c
@@ -33,9 +33,10 @@ void menu(){
 
 
 int main(){
+
     while(1){
         clear_screen();
-        char *cpuInfo = get_cpu_info()
+        char *cpuInfo = get_cpu_info();
         menu();
         sleep(3);
     }
