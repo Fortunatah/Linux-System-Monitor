57d6f75e07ffe4fbb311e54a1813be218d593051
Fortunatah <haydenjohnfortunata@gmail.com>
2026-01-31 10:24:28 -0500

    substring

--- include/get_system_info.c
+++ include/get_system_info.c
@@ -43,6 +43,7 @@ int get_line_total( char *pass){
     int count = 0;
     int i = 0;
     while((c = pass[i]) != '\0'){
+        printf("here\n");
         if((c = pass[i]) == ' '){
             printf("add=%s" , add);
             //num = strol(add , NULL , 10 ); // conver to int
