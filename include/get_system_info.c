f84f0772a25e5d14a357c402a4423189bdaa0d98
Fortunatah <haydenjohnfortunata@gmail.com>
2026-02-01 17:26:08 -0500

    Started uptime

--- include/get_system_info.c
+++ include/get_system_info.c
@@ -207,7 +207,7 @@ char *get_uptime(){
     int character;
     char uptimeNumber[64];
     int count = 0;
-    while ((character = fgetc(file)) != ' ') {
+    while(isdigit(character = fgetc(file))) {
         printf("%c", character);
     }
 
